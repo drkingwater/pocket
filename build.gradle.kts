964ae59e9dc155be8c7ae95657a22144aeb7155b
pxq <hao15296802206@gmail.com>
2022-03-29 23:26:38 +0800

    配置运行环境、插件版本等信息

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,23 @@
+import java.util.*
+import java.io.File
+
+// 配置local properties
+val localProperties = Properties().apply {
+    load(File(rootDir, "local.properties").inputStream())
+}
+
+// 先找gradle.properties再找local.properties
+fun properties(key: String) = project.findProperty(key)?.toString() ?: localProperties.getProperty(key)
+
+
 plugins {
     id("org.jetbrains.intellij") version "1.4.0"
     kotlin("jvm") version "1.5.10"
     java
 }
 
-group = "me.pxq.asplugin"
-version = "1.0-SNAPSHOT"
+group = properties("pluginGroup")
+version = properties("pluginVersion")
 
 repositories {
     mavenCentral()
@@ -19,15 +31,28 @@ dependencies {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version.set("2021.3.1")
+    pluginName.set(properties("pluginName"))
+    version.set(properties("platformVersion"))
+    type.set(properties("platformType"))
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
 }
 tasks {
     patchPluginXml {
+        version.set(properties("pluginVersion"))
+        sinceBuild.set(properties("pluginSinceBuild"))
+        untilBuild.set(properties("pluginUntilBuild"))
         pluginDescription.set("a website pocket")
         changeNotes.set("""
             Add change notes here.<br>
             <em>most HTML tags may be used</em>        """.trimIndent())
     }
+
+    runIde {
+        ideDir.set(File(properties("androidStudioPath")))
+    }
+
 }
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
